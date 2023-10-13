a15ff67bb770ab588ea8f7a70e9a07124104e822
Bearcin46 <imsweety33@gmail.com>
2023-10-13 17:58:00 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,8 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'color-patten':"linear-gradient(to_right_bottom,rgba(49,84,44,0.8),rgba(16,71,52,0.8)), url('/public/images/banner.jpg')",
-        'hero-pattern': " url('/public/images/banner.jpg')",
+        
+        'hero-pattern': " url('/public/images/b.jpeg')",
      },
     },
   },
